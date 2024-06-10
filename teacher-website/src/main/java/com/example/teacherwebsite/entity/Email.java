2c10dcd47ff79a90cfb7ce60460cbb022e6146dc
Alanz <zienzeng0510@gmail.com>
2024-06-10 15:01:53 -0700

    add some new thing

--- teacher-website/src/main/java/com/example/teacherwebsite/entity/Email.java
+++ teacher-website/src/main/java/com/example/teacherwebsite/entity/Email.java
@@ -3,6 +3,7 @@ package com.example.teacherwebsite.entity;
 public class Email {
     private Long id;
     private String email;
+    private String name;
 
     public void setId(Long id) {
         this.id = id;

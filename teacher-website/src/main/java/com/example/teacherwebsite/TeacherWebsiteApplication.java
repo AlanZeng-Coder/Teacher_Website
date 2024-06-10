44257cb3d30bf699d11de74a818dcc8e902c11f4
Alanz <zienzeng0510@gmail.com>
2024-06-10 15:46:57 -0700

    more better

--- teacher-website/src/main/java/com/example/teacherwebsite/TeacherWebsiteApplication.java
+++ teacher-website/src/main/java/com/example/teacherwebsite/TeacherWebsiteApplication.java
@@ -2,8 +2,10 @@ package com.example.teacherwebsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.example.teacherwebsite.mapper")
 public class TeacherWebsiteApplication {
 
     public static void main(String[] args) {

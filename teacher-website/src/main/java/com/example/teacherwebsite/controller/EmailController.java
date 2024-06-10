d866b2cc1ecc7555bbf994cd1e17f07e1b174b65
Alanz <zienzeng0510@gmail.com>
2024-06-10 16:17:42 -0700

    new index

--- teacher-website/src/main/java/com/example/teacherwebsite/controller/EmailController.java
+++ teacher-website/src/main/java/com/example/teacherwebsite/controller/EmailController.java
@@ -1,5 +1,6 @@
 package com.example.teacherwebsite.controller;
 
+import com.example.teacherwebsite.entity.Email;
 import com.example.teacherwebsite.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,13 +9,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class EmailController {
     @Autowired
     private EmailService emailService;
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        List<Email> emails = emailService.getAllEmails();
+        model.addAttribute("emails", emails);
         return "index";
     }
 
